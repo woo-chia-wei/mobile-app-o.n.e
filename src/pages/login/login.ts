b5573f0d4a1af5b4d96f11e91e7a3a5d50714e6a
unknown <chiaweiwoo123@gmail.com>
2018-04-07 21:52:40 +0800

    Moved authentication & db call to user service.

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, LoadingController, ToastController } from 'ionic-angular';
 import { User } from "../../models/user";
-import { AngularFireAuth } from 'angularfire2/auth';
 import { AccountPage } from '../account/account';
-import { AngularFireDatabase } from 'angularfire2/database';
+import { UserServiceProvider } from '../../providers/user-service/user-service';
 
 @IonicPage()
 @Component({
@@ -14,28 +13,25 @@ export class LoginPage {
 
   user = {} as User;
 
-  constructor(private afAuth: AngularFireAuth,
-            public navCtrl: NavController, 
-            public navParams: NavParams,
-            public loadingController: LoadingController,
-            public toastController: ToastController,
-            public db: AngularFireDatabase) {
+  constructor(public navCtrl: NavController, 
+              public navParams: NavParams,
+              public loadingController: LoadingController,
+              public toastController: ToastController,
+              public userService: UserServiceProvider) {
   }
  
   async login(user: User) {
     try {
-      const result = await this.afAuth.auth.signInWithEmailAndPassword(user.email, user.password);
+      const result = await this.userService.loginUser(user.email, user.password);
+
       if (result) {
 
-        let loader = this.loadingController.create({
+        this.loadingController.create({
           content: "Please wait...",
           duration: 3000
         });
 
-        this.db.list('logins').push({
-          user: this.afAuth.auth.currentUser.email,
-          loginTime: Date.now()
-        });
+        this.userService.logUserLogin();
 
         console.log("Login account successfully.");
 
@@ -44,13 +40,12 @@ export class LoginPage {
     }
     catch (e) {
 
-      let toast = this.toastController.create({
+      this.toastController.create({
         message: e,
         duration: 3000,
         position: 'top'
-      });
-      
-      toast.present();
+      }).present();
+
       user.password = "";
 
       console.error("login failure", e);
@@ -59,40 +54,32 @@ export class LoginPage {
  
   async register(user: User) {
     try {
-      const result = await this.afAuth.auth.createUserWithEmailAndPassword(
-        user.email,
-        user.password
-      );
+      const result = await this.userService.registerUser(user.email, user.password);
       if (result) {
 
-        let loader = this.loadingController.create({
+        this.loadingController.create({
           content: "Please wait...",
           duration: 3000
         });
 
-        let toast = this.toastController.create({
+        this.toastController.create({
           message: "Account is created successfully.",
           duration: 1000,
           position: 'middle'
-        });
-        
-        toast.present();
+        }).present();
 
         console.log("Register account successfully.");
-
-        this.afAuth.auth.currentUser
 
         this.navCtrl.setRoot(AccountPage);
       }
     } catch (e) {
       
-      let toast = this.toastController.create({
+      this.toastController.create({
         message: e,
         duration: 3000,
         position: 'top'
-      });
-      
-      toast.present();
+      }).present();
+
       user.password = "";
 
       console.error("Register failure", e);
--- src/providers/user-service/user-service.ts
+++ src/providers/user-service/user-service.ts
@@ -1,17 +1,31 @@
-import { HttpClient } from '@angular/common/http';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { AngularFireDatabase } from 'angularfire2/database';
 import { Injectable } from '@angular/core';
 
-/*
-  Generated class for the UserServiceProvider provider.
-
-  See https://angular.io/guide/dependency-injection for more info on providers
-  and Angular DI.
-*/
 @Injectable()
 export class UserServiceProvider {
 
-  constructor(public http: HttpClient) {
+  constructor(private afAuth: AngularFireAuth,
+              public db: AngularFireDatabase) {
     console.log('Hello UserServiceProvider Provider');
   }
 
+  public loginUser(email: string, password: string){
+    return this.afAuth.auth.signInWithEmailAndPassword(email, password);
+  }
+
+  public registerUser(email: string, password: string){
+    return this.afAuth.auth.createUserWithEmailAndPassword(email, password);
+  }
+
+  public getCurrentUserEmail(): string{
+    return this.afAuth.auth.currentUser.email;
+  }
+
+  public logUserLogin(){
+    this.db.list('logins').push({
+      user: this.afAuth.auth.currentUser.email,
+      loginTime: Date.now()
+    });
+  }
 }
