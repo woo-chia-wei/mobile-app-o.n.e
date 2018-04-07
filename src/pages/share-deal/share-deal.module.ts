fa20a42d0de5ac870d71dcc895414cc26b56d73d
unknown <chiaweiwoo123@gmail.com>
2018-04-07 20:22:09 +0800

    Created pages & providers.

---
+++ src/pages/share-deal/share-deal.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { ShareDealPage } from './share-deal';
+
+@NgModule({
+  declarations: [
+    ShareDealPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(ShareDealPage),
+  ],
+})
+export class ShareDealPageModule {}
