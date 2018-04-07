c5cf2f168a305122ddb52b0d3f25af5edcf5a50e
unknown <chiaweiwoo123@gmail.com>
2018-04-07 20:19:32 +0800

    Added firebase credentials.

---
+++ src/app/firebase.credentials.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CREDENTIALS = {
+    apiKey: "AIzaSyBHY8xa-Rycqf__UeoGk1kfxksfwed-MsM",
+    authDomain: "mobile-assignment-fa43d.firebaseapp.com",
+    databaseURL: "https://mobile-assignment-fa43d.firebaseio.com",
+    projectId: "mobile-assignment-fa43d",
+    storageBucket: "mobile-assignment-fa43d.appspot.com",
+    messagingSenderId: "516750291967"
+  };
