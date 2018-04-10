5b9de2eb4f9bc41252966c17fcef22313e17b7da
Woo Chia Wei <chiaweiwoo123@gmail.com>
2018-04-11 00:36:27 +0800

    Added distance formula in map service.

--- src/providers/google-map-service/google-map-service.ts
+++ src/providers/google-map-service/google-map-service.ts
@@ -24,5 +24,27 @@ export class GoogleMapServiceProvider {
     return response['results'][0]['geometry']['location'];
   }
 
+  //Source of formula:
+  //https://stackoverflow.com/questions/27928/calculate-distance-between-two-latitude-longitude-points-haversine-formula/27943#27943
+  public getDistanceInKM(lat1: number, lng1: number, lat2: number, lng2: number) {
+    var R = 6371; // Radius of the earth in km
+    var dLat = this.deg2rad(lat2-lat1);
+    var dLon = this.deg2rad(lng2-lng1); 
+    var a = 
+      Math.sin(dLat/2) * Math.sin(dLat/2) +
+      Math.cos(this.deg2rad(lat1)) * Math.cos(this.deg2rad(lat2)) * 
+      Math.sin(dLon/2) * Math.sin(dLon/2)
+      ; 
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+    var d = R * c; // Distance in km
+    return d;
+  }
+
+  private deg2rad(deg: number) {
+    return deg * (Math.PI/180)
+  }
+
+  
+
 
 }
